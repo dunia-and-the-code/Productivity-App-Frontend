b3932a7cd5204961630c417d80d00b58375ce6ab
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-06-21 08:10:31 +0100

    Thursday update

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-//live update time and date//////////////////////
-//Daily quotes pulle din from API///////////////
-//To Do list - checkboxes and create form
 //Pomodoro clock - excuses alert
+//increment / decrement pomodoro timer
 //Excuses redered in excuses card with timestamps
 //Daily journal rendered with time stamp
 //Daily journal form avaliable once a day
 //Journal posts are editable
+//Journal posts listed with 'Readom more' button
+//'Journal entry' button to toggle submition form
 //CSS
-//Object Oriented JavaScript
+
 
 const userId = 2
 
 const tasksURL = 'http://localhost:3000/tasks'
 const quotesURL = 'http://quotes.rest/qod.json'
+const journalPostsURL = 'http://localhost:3000/journal_posts'
 
 const toDoList = document.querySelector('#to_do_list')
 const toDoForm = document.querySelector('#to_do_form')
 const input = document.querySelector('#description')
 const quotesContainer = document.querySelector('#quotes_container')
+const sessionTime = document.querySelector('#session_time')
+const resetBtn = document.querySelector('#reset_btn')
+const sessionStartBtn = document.querySelector('#session_start_btn')
+const breakStartBtn = document.querySelector('#break_start_btn')
+const breakTime = document.querySelector('#break_time')
+const journalContainer =  document.querySelector('#daily_journal')
 
 
 function updateDateTime() {
@@ -31,10 +38,10 @@ function updateDateTime() {
     let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     let year = now.getFullYear()
 
-    let fullDateTime = `${hour}:${minutes}<br>${date} ${day[now.getDay()]} ${month[now.getMonth()]} ${year}`
+    let fullDateTime = `${hour}:${minutes}<br>${date} ${day[now.getDay()-1]} ${month[now.getMonth()]} ${year}`
 
     dateTimetag.innerHTML = fullDateTime
-    setTimeout(updateDateTime, 100)
+    setTimeout('updateDateTime()', 100)
 
 }
 
@@ -50,8 +57,11 @@ function getDailyQuote() {
 
 function renderQuote(quote) {
 
-    const quoteContent = document.createElement('h3')
+    const quoteContent = document.createElement('p')
     const quoteAuthor = document.createElement('p')
+
+    quoteContent.setAttribute('id', 'quoteContent')
+    quoteAuthor.setAttribute('id', 'quoteAuthor')
 
     quoteContent.innerText = `"${quote.contents.quotes[0].quote}"`
     quoteAuthor.innerText += quote.contents.quotes[0].author
@@ -74,21 +84,20 @@ function getTasks() {
 function renderTask(task) {
 
     const taskEl = document.createElement('li')
-    // debugger
-    console.log(task)
+    
     taskEl.setAttribute('id', 'task_el')
     taskEl.dataset.id = task.id
     taskEl.innerHTML = task.description
 
     const checkBox = document.createElement('input')
     checkBox.setAttribute('type', 'checkbox')
-    checkBox.setAttribute('id', 'checkbox')
+    checkBox.setAttribute('class', 'checkbox')
 
     taskEl.append(checkBox)
     toDoList.append(taskEl)
 
     checkBox.addEventListener('click', () => {
-        // debugger
+        
         const task = checkBox.parentNode
         const taskId = task.dataset.id
         task.parentNode.removeChild(task)
@@ -109,6 +118,16 @@ function renderTasks(tasks) {
 
 }
 
+const addTaskBtn = document.querySelector('#add_task_btn')
+const toDoContainer = document.querySelector('#to_do_container')
+const toDoCard = document.querySelector('#to_do_card')
+const formContainer = document.querySelector('#form_container')
+
+addTaskBtn.addEventListener('click', () => {
+    
+    toDoContainer.innerHTML = ''
+    toDoContainer.append(formContainer)
+})
 
 function deleteTaskFromServer(taskId) {
 
@@ -128,9 +147,12 @@ toDoForm.addEventListener('submit', (e) => {
         user_id: userId
     }
 
-    console.log(task)
     createTaskOnServer(task)
+    
+    .then(toDoForm.reset())
 
+    toDoContainer.innerHTML = ''
+    toDoContainer.append(toDoCard)
 })
 
 
@@ -145,6 +167,189 @@ function createTaskOnServer(task) {
         .then(task => renderTask(task))
 }
 
+
+let sessionMinutes = 26
+
+function setSessionMinutes() {
+   
+    if( sessionMinutes > 0 ){
+
+        sessionMinutes--
+        breakTime.innerHTML = `${sessionMinutes}:${sessionSeconds}`
+        breakTime.setAttribute('id', 'session_time')
+
+    }else{
+
+        breakTime.innerHTML = "Session over, well done!"
+
+    }
+}
+
+
+let sessionSeconds = 60
+
+function setSessionSeconds() {
+
+    if( sessionSeconds > 0 ){
+        
+        sessionSeconds--
+
+        if( sessionSeconds < 10 ) {
+            breakTime.innerHTML = `${sessionMinutes}:0${sessionSeconds}`
+        }else{
+            breakTime.innerHTML = `${sessionMinutes}:${sessionSeconds}`
+        }
+    }else{
+
+        sessionSeconds = 60
+
+    }
+}
+
+sessionTime.innerHTML = `${sessionMinutes}:${sessionSeconds}`
+
+sessionStartBtn.addEventListener('click', () => {
+    
+    setInterval('setSessionMinutes()', 60000)
+    setInterval('setSessionSeconds()', 1000)
+
+})
+
+
+let breakMinutes = 26
+
+function setBreakMinutes() {
+   
+    if( breakMinutes > 0 ){
+
+        breakMinutes--
+        breakTime.innerHTML = `${breakMinutes}:${breakSeconds}`
+        breakTime.setAttribute('id', 'break_time')
+
+    }else{
+
+        breakTime.innerHTML = "Break over, back to work."
+
+    }
+}
+
+
+let breakSeconds = 60
+
+function setBreakSeconds() {
+
+    if( breakSeconds > 0 ){
+        
+        breakSeconds--
+
+        if( breakSeconds < 10 ) {
+            breakTime.innerHTML = `${breakMinutes}:0${breakSeconds}`
+        }else{
+            breakTime.innerHTML = `${breakMinutes}:${breakSeconds}`
+        }
+    }else{
+
+        breakSeconds = 60
+
+    }
+}
+
+breakTime.innerHTML = `${breakMinutes}:${breakSeconds}`
+
+breakStartBtn.addEventListener('click', () => {
+    
+    setInterval('setBreakMinutes()', 60000)
+    setInterval('setBreakSeconds()', 1000)
+
+})
+
+function getJournalPosts() {
+
+    fetch(journalPostsURL)
+    .then(resp => resp.json())
+    .then(json => renderJournalPosts(json))
+}
+
+
+function renderJournalPosts(posts) {
+
+    posts.forEach(post => renderJournalPost(post))
+
+}
+
+
+function renderJournalPost(post) {
+
+    const postEl = document.createElement('li')
+    postEl.setAttribute('id', 'post_el')
+    postEl.dataset.id = post.id
+    postEl.innerHTML = post.description
+
+    const editBtn = document.createElement('button')
+    editBtn.setAttribute('id', 'edit_btn')
+    editBtn.innerHTML = 'Edit'
+
+    postEl.append(editBtn)
+    journalContainer.append(postEl)
+    
+    editBtn.addEventListener('click', (e) => editPostForm(e, post))
+
+}
+
+function editPostForm(e, post) {
+
+    const editForm = document.createElement('form')
+    const editInput = document.createElement('textarea')
+    const currentPost =  e.target.parentNode
+    const submitEditBtn = document.createElement('button')
+
+    submitEditBtn.innerHTML = 'Submit'
+
+    editForm.append(editInput)
+    editForm.append(submitEditBtn)
+    currentPost.append(editForm)
+    
+    editForm.addEventListener('submit', (e) => {
+        
+        e.preventDefault()
+        
+        post.description = editInput.value
+         
+        currentPost.innerHTML = post.description
+        const editBtn = document.createElement('button')
+        
+        editBtn.setAttribute('id', 'edit_btn')
+        
+        editBtn.innerHTML = 'Edit'
+        currentPost.append(editBtn)
+
+        editBtn.addEventListener('click', (e) => editPostForm(e, post))
+        
+        editPostOnServer(post)
+    
+    })
+}
+
+
+function editPostOnServer(post) {
+debugger
+    return fetch(journalPostsURL + `/${post.id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(post)
+    })
+    .then(resp => resp.json())
+}
+
+
+function editPost(post) {
+
+
+}
+
 updateDateTime()
 getDailyQuote()
 getTasks()
+getJournalPosts()
+
+
